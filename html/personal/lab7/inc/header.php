88a6e6101e4d641de66be1facf7f1fd193d7959e
Hayata Suenaga <hayata.suenaga@centre.edu>
2021-11-23 21:48:53 +0000

    Complete lab8

--- html/personal/lab7/inc/header.php
+++ html/personal/lab7/inc/header.php
@@ -3,6 +3,9 @@
 <head>
   <title>Lab 7</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <?php if(isset($_COOKIE['mode']) && $_COOKIE['mode'] === 'dark'): ?>
+    <link rel="stylesheet" href="dark.css">
+  <?php endif; ?>
 </head>
 <body>
   <header class="d-flex flex-wrap justify-content-center px-5 py-3 mb-4 border-bottom">
@@ -16,3 +19,14 @@
     </ul> -->
   </header>
   <div class="container">
+    <?php if(isset($_COOKIE['mode']) && $_COOKIE['mode'] === 'dark'): ?>
+      <h4>Currently on Dark Mode</h4>
+      <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+        <button class="btn btn-primary" type="submit" name="operation" value="set_light_mode">Change to Light Mode</button>
+      </form>
+    <?php else: ?>
+      <h4>Currently on Light Mode</h4>
+      <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+        <button class="btn btn-primary" type="submit" name="operation" value="set_dark_mode">Change to Dark Mode</button>
+      </form>
+    <?php endif; ?>
--- html/personal/lab7/manageInstruments.php
+++ html/personal/lab7/manageInstruments.php
@@ -1,6 +1,8 @@
 <?php
     /* Configure the error level */
     require('./config/error.php');
+    /* Start a session */
+    session_start();
     /* Get the connect function */
     require('./config/db.php');
     /* Connect to the instrument_rentals table */
@@ -11,10 +13,13 @@
     if (!$result) die ($conn->error);
     $instruments = $result->fetch_all(MYSQLI_ASSOC);
 
+    //TODO: Account for the usual get request 
     switch ($_POST['operation'] ?? 'get_all') {
       case 'delete_selected_rows':
         /* Query for deleting a specified instrument */
         $query = "DELETE FROM instruments WHERE instrument_id = ?";
+        /* Declare a variable for counting the number of records to be deleted */
+        $num_of_deleted_records = 0;
         /* Iterate over all instruments in the instruments table */
         foreach($instruments as $instrument) {
           /* Get the id of the instrument being iterated */
@@ -24,8 +29,12 @@
             $stmt = $conn->prepare($query);
             $stmt->bind_param('i', $id);
             if (!$stmt->execute()) die($conn->error);
+            /* Increment the number of records deleted */
+            $num_of_deleted_records += 1;
           }
         }
+        /* Add the number of records deleted to the count session variable */
+        $_SESSION['del_count'] += $num_of_deleted_records;
         /* Redirect the user to the original url */
         header("Location: {$_SERVER['PHP_SELF']}", true, 303);
         exit();
@@ -42,12 +51,59 @@
           ('Trombone'),
           ('Keyboard');";
         if (!$conn->query($query)) die($conn->error);
+
         header("Location: {$_SERVER['PHP_SELF']}", true, 303);
         exit();
 
       case 'delete_all': /* Delete all rows from the instrument table */
+        /* Get the number of instruments currently on database.
+         * This number is addd to the session variable that keeps trakc of
+         * the number of recorded deleted */
+        $query = 'SELECT count(*) FROM instruments';
+        $result = $conn->query($query);
+        if (!$result) die($conn->error);
+        $count = $result->fetch_all(MYSQLI_NUM)[0][0];
+
+        /* Update the session variable for the count if a user is logged in */
+        if (isset($_SESSION['del_count']))
+          $_SESSION['del_count'] += $count;
+
+        /* Delete all instruments from the database */
         $query = 'DELETE FROM instruments';
         if (!$conn->query($query)) die ($conn->error);
+
+        header("Location: {$_SERVER['PHP_SELF']}", true, 303);
+        exit();
+
+      case 'sign_in':
+        /* Get the user name from the request */
+        $user_name = $_POST['user_name'];
+        /* Set the count variable for the new user to zero */
+        $_SESSION['user_name'] = $user_name;
+        $_SESSION['del_count'] = 0;
+
+        header("Location: {$_SERVER['PHP_SELF']}", true, 303);
+        exit();
+
+      case 'sing_out':
+        /* Remove the count variable for the requested user */
+        unset($_SESSION['user_name']);
+        unset($_SESSION['del_count']);
+
+        header("Location: {$_SERVER['PHP_SELF']}", true, 303);
+        exit();
+
+      case 'set_light_mode':
+        /* Set the mode cookie variable to light */
+        setcookie('mode', 'light');
+
+        header("Location: {$_SERVER['PHP_SELF']}", true, 303);
+        exit();
+
+      case 'set_dark_mode';
+        /* Send the mode cookie variable to dark */
+        setcookie('mode', 'dark');
+
         header("Location: {$_SERVER['PHP_SELF']}", true, 303);
         exit();
 
@@ -59,7 +115,24 @@
     $conn->close();
 ?>
 
+<!-- Import the header --->
 <?php require('./inc/header.php') ?>
+
+<?php if(!isset($_SESSION['user_name'])): ?>
+  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+    <div class="form-group">
+      <label>Your name</label>
+      <input type="text" class="form-control" name="user_name" />
+    </div>
+    <button type="submit" class="btn btn-primary" name="operation" value="sign_in">Sign in</button>
+  </form>
+<?php else: ?>
+  <h3><?php echo 'Welcome, ' . htmlentities($_SESSION['user_name']) . '!'; ?></h3>
+  <p><?php echo "You have deleted {$_SESSION['del_count']} records"; ?></p>
+  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+    <button type="submit" class="btn btn-primary" name="operation" value="sing_out">Sign out</button>
+  </form>
+<?php endif; ?>
 
 <form id="delete_selection" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
   <table class="table">
